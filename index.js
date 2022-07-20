67236a4221155733791598d35c408c19e8a50027
waasike <mitchellewasikee@gmail.com>
2022-07-20 16:14:22 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+// QUESTION 1
+let student_score = 72;
+
+if (student_score > 75){
+    console.log("Grade A")
+}
+else if (student_score >=65 && student_score <=74){
+    console.log("Grade B")
+}
+else if (student_score >=55 && student_score <= 64){
+    console.log("Grade C")
+}
+else if (student_score >=45 && student_score<= 54){
+    console.log("Grade D")
+}
+else{
+    console.log("FAIL")
+}
+
+// QUESTION 2
+console.log("YOU ARE IN THE MIDDLE OF A FOREST, ON THE LEFT YOUR YOUNGER SISTER IS BEING CHASED BY A LION AND SCREAMING FOR YOUR HELP! AND ON THE RIGHT IS A POOL WERE YOU CAN SWIM AWAY. ")
+console.log("Which side do you go? Left or Right?");
+let answer = "left";
+
+if (answer= "left"){
+    console.log("You have succesfully manged to save your sister and hide from the lion, thank you for your bravery!")
+}
+else if (answer= "right"){
+    console.log("Sorry, you were eaten by a crocodile in the pool and your sister hates you!")
+}
+
+// solution for q2 will be better once I learn how to get user input!
